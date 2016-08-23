20dddbda978e58e39919336c69757811ffe85a4b
nderaad <nickderaad@gmail.com>
2016-08-23 04:35:27 +0000

    Added drink names

--- pirate_bartender.py
+++ pirate_bartender.py
@@ -16,30 +16,34 @@ ingredients = {
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
 }
 
+adjectives = ["crooked", "cruel","cumbersome","delightful","depraved","diminutive","dilapidated",]
+nouns = ["snake", "horse","mirror","bathwater","goo","hair","tire",]
+
 responses = {}
 
-def type_of_drink():
-    for key in questions:
-        inp = input(questions[key].lower())
-        if inp == "yes" or inp == "y":
-            response = True
-        elif inp == "no" or inp == "n":
-            response = False
-        else:
-            print("You need to say Yes or No")
-        responses[key] = response
-        print(inp)
-    print(responses)
+if __name__ == '__main__':
+    
+    def type_of_drink():
+        for key in questions:
+            inp = input(questions[key].lower())
+            if inp == "yes" or inp == "y":
+                response = True
+            elif inp == "no" or inp == "n":
+                response = False
+            else:
+                print("You need to say Yes or No")
+            responses[key] = response
 
-type_of_drink()
-
-def make_drink(x):
-    drink = []
-    for key in ingredients:
-        if x[key] == True:
-            ingredient = random.choice(ingredients[key])
-        elif x[key] == False:
-            ingredient = "Nothing Added"
-        drink.append(ingredient)
-    print(drink)
-make_drink(responses)
+    type_of_drink()
+    
+    def make_drink(x):
+        drink = []
+        for key in ingredients:
+            if x[key] == True:
+                ingredient = random.choice(ingredients[key])
+            elif x[key] == False:
+                ingredient = "Nothing Added"
+            drink.append(ingredient)
+        print("Here's your {} {}. It's made with a {} enjoy... Oh and ARRGGGHH, cuz we're pirates I guess.".format(random.choice(adjectives),random.choice(nouns),print(drink)))
+    
+    make_drink(responses)
